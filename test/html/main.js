ef0f73217f2e3011ff603c222dbd80c7d9a08485
charles <charles.braquet@e.email>
2024-01-06 15:07:27 +0100

    formatting

--- test/html/main.js
+++ test/html/main.js
@@ -3,7 +3,7 @@
 import { trapeze_rod_svg } from './tmp/svg-bundle.js';
 
 console.log('main.js says hello!');
-const elem1 = document.getElementById("art1");
+const elem1 = document.getElementById('art1');
 //elem1.append('boo');
 //elem1.replaceChildren('bee');
 elem1.replaceChildren(trapeze_rod_svg);
